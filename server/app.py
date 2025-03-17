7bcfac9b750d815adf28c7ec291f6680f87d00aa
wondieki <wkerubo28@gmail.com>
2025-03-17 12:35:31 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
-
 from models import db, Message
 
 app = Flask(__name__)
@@ -11,45 +10,42 @@ app.json.compact = False
 
 CORS(app)
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
 @app.route('/messages', methods=['GET', 'POST'])
 def messages():
     if request.method == 'GET':
-        messages = Message.query.order_by('created_at').all()
-        
-        return make_response([message.to_dict() for message in messages],200 )
-    
+        messages = Message.query.order_by(Message.created_at).all()
+        print("Fetched Messages:", messages)  # Debugging print
+        return jsonify([message.to_dict() for message in messages]), 200
+
     elif request.method == 'POST':
         data = request.get_json()
-        message = Message(
+        new_message = Message(
             body=data['body'],
             username=data['username']
         )
-
-        db.session.add(message)
+        db.session.add(new_message)
         db.session.commit()
-
-        return  make_response(message.to_dict(),  201,)
-
+        return jsonify(new_message.to_dict()), 201
 
 @app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
     message = Message.query.filter_by(id=id).first()
+    if not message:
+        return jsonify({"error": "Message not found"}), 404
 
     if request.method == 'PATCH':
         data = request.get_json()
         for attr in data:
             setattr(message, attr, data[attr])
-            
-        db.session.add(message)
         db.session.commit()
-
-        return make_response(message.to_dict(),200 )
+        return jsonify(message.to_dict()), 200
 
     elif request.method == 'DELETE':
         db.session.delete(message)
         db.session.commit()
+        return jsonify({"deleted": True}), 200
 
-        return make_response( {'deleted': True} , 200)
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
